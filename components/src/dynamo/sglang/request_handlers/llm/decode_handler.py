82438e580bae73741a31b7ab319716e3b992dac2
Vladislav Nosivskoy <vladnosiv@gmail.com>
2026-01-30 19:18:43 +0300

    try fix 128 max new tokens
    Signed-off-by: Vladislav Nosivskoy <vladnosiv@gmail.com>

--- components/src/dynamo/sglang/request_handlers/llm/decode_handler.py
+++ components/src/dynamo/sglang/request_handlers/llm/decode_handler.py
@@ -72,7 +72,7 @@ class DecodeWorkerHandler(BaseWorkerHandler):
                 "temperature": sampling_opts.get("temperature"),
                 "top_p": sampling_opts.get("top_p"),
                 "top_k": sampling_opts.get("top_k"),
-                "max_new_tokens": stop_conditions.get("max_tokens", 32000),
+                "max_new_tokens": 32768,  # stop_conditions.get("max_tokens"),
                 "ignore_eos": stop_conditions.get("ignore_eos"),
             }
         else:
